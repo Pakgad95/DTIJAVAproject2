84772327343c42d76049180dc69e0bc4980e2e29
Pakgad95 <s6752410029@sau.ac.th>
2025-02-05 01:48:58 +0700

    DTI project2 เพิ่มเติม

---
+++ src/flowcontrol/MyClassCF03.java
@@ -0,0 +1,29 @@
+package flowcontrol;
+
+public class MyClassCF03 {
+    //กลุ่มที่ 2 Loop (while, do-while, for)
+    public static void main(String[] args) {
+        //while
+        int i = 1;
+
+        while( i <= 5 ){
+            System.out.println("Hi...");
+            i = i + 1; //i!! หรือ i != 1
+        }
+
+        System.out.println("-------------------");
+        //do-while
+        int j = 1;
+        do {
+            System.out.println("Hey...");
+            j = j + 1;
+        }while(  j <= 5 );
+
+        System.out.println("-------------------");
+        //for
+        for( int k = 1; k <= 5; k = k + 1){
+            System.out.println("Hoo...");
+
+        }
+    }
+}
